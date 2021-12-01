e21ff0fee6791ed8ca4ac15760a5ff1ab07efcd5
Hawk1401 <mail@florian-ko.ch>
2021-12-01 17:49:41 +0100

    First commit

---
+++ Helper/ConsoleHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Helper
+{
+    public class ConsoleHelper
+    {
+        public static void PrintTest(long result, long expected)
+        {
+            if(result == expected)
+            {
+                Console.WriteLine("The test was successfully");
+            }
+            else
+            {
+                Console.WriteLine("The test was notsuccessfully");
+            }
+
+            Console.WriteLine($"The Test returned {result}, expected was {expected}");
+
+        }
+
+        public static void PrintFirstResult(long result)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"The result Of the First Problem is {result}");
+            Console.WriteLine("#############################################");
+            Console.WriteLine();
+
+
+
+        }
+
+        public static void PrintSecondResult(long result)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"The result Of the Second Problem is {result}");
+        }
+    }
+}
---
+++ Helper/IDay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public interface IDay
+    {
+
+        public void Run(int TestResultPartOne)
+        {
+            var Testnums = File.ReadAllLines("testInput.txt").Select(x => int.Parse(x)).ToList();
+            var TestReult = PartOne(Testnums);
+            ConsoleHelper.PrintTest(TestReult, TestResultPartOne);
+
+
+            var nums = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+            ConsoleHelper.PrintFirstResult(PartOne(nums));
+        }
+
+        public void Run(int TestResultPartOne, int TestResultPartTwo)
+        {
+            var Testnums = File.ReadAllLines("testInput.txt").Select(x => int.Parse(x)).ToList();
+            var TestReult = PartOne(Testnums);
+            ConsoleHelper.PrintTest(TestReult, TestResultPartOne);
+
+
+            var nums = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+            ConsoleHelper.PrintFirstResult(PartOne(nums));
+
+            //Part Two 
+            var TestReultPartTwo = PartTwo(Testnums);
+            ConsoleHelper.PrintTest(TestReultPartTwo, 5);
+            ConsoleHelper.PrintSecondResult(PartTwo(nums));
+        }
+
+
+        public int PartOne(List<int> nums);
+        public int PartTwo(List<int> nums);
+
+    }
+}
